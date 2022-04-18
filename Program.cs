d4f45d663a955015d714a2515dd16ac5afb6015f
Stephen Cunningham <stevecunningham@outlook.com>
2022-04-18 00:08:49 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace WeatherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.WriteLine("What is your API Key");
+            var apiKey = Console.ReadLine();    
+
+            Console.WriteLine("Please eneter city name");
+            var cityName = Console.ReadLine();  
+            var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+
+            var weatherData = client.GetStringAsync(weatherUrl).Result;
+            var response = JObject.Parse(weatherData).GetValue("main").ToString();
+            Console.WriteLine($"The weather in {cityName} is {response} ");
+        }
+    }
+}
